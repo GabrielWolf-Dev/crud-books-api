dad54d3f1e187fb347a5f8381d811e6e7a165feb
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2023-07-31 13:35:08 -0300

    fix: cant set headers after they are sent to the client

--- middlewares/booksMiddleware.js
+++ middlewares/booksMiddleware.js
@@ -1,65 +1,49 @@
-const validateUndefinedFields = (res, fields, isReady) => {
-  if (fields.title === undefined) {
-    isReady = false;
+const validateBodyReq = (req, res, next) => {
+  const { title, image, pages, author, publisher, rating } = req.body;
 
+  if (title === undefined) {
     return res
       .status(400)
       .json({ status: 400, message: "The field title is required" });
   }
 
-  if (fields.image === undefined) {
-    isReady = false;
-
+  if (image === undefined) {
     return res
       .status(400)
       .json({ status: 400, message: "The field image is required" });
   }
 
-  if (fields.pages === undefined) {
-    isReady = false;
-
+  if (pages === undefined) {
     return res
       .status(400)
       .json({ status: 400, message: "The field pages is required" });
   }
 
-  if (fields.author === undefined) {
-    isReady = false;
-
+  if (author === undefined) {
     return res
       .status(400)
       .json({ status: 400, message: "The field author is required" });
   }
 
-  if (fields.publisher === undefined) {
-    isReady = false;
-
+  if (publisher === undefined) {
     return res
       .status(400)
       .json({ status: 400, message: "The field publisher is required" });
   }
 
-  if (fields.rating === undefined) {
-    isReady = false;
-
+  if (rating === undefined) {
     return res
       .status(400)
       .json({ status: 400, message: "The field rating is required" });
   }
-};
 
-const validateEmptyFields = (res, fields, isReady) => {
-  if (fields.title === "") {
-    isReady = false;
-
+  if (title === "") {
     return res
       .status(400)
       .json({ status: 400, message: "Title cannot be empty" });
   }
 
-  if (fields.pages <= 5) {
-    isReady = false;
-
+  if (pages <= 5) {
     return res.status(400).json({
       status: 400,
       message:
@@ -67,89 +51,61 @@ const validateEmptyFields = (res, fields, isReady) => {
     });
   }
 
-  if (fields.author === "") {
-    isReady = false;
-
+  if (author === "") {
     return res
       .status(400)
       .json({ status: 400, message: "Author cannot be empty" });
   }
 
-  if (fields.publisher === "") {
-    isReady = false;
-
+  if (publisher === "") {
     return res
       .status(400)
       .json({ status: 400, message: "Publisher cannot be empty" });
   }
 
-  if (fields.rating === 0) {
-    isReady = false;
-
+  if (rating === 0) {
     return res
       .status(400)
       .json({ status: 400, message: "Enter a rating greater than 0" });
   }
-};
-
-const validateBodyReq = (req, res, next) => {
-  const { title, image, pages, author, publisher, rating } = req.body;
-  let isReady = true;
-
-  validateUndefinedFields(res, req.body, isReady);
-  validateEmptyFields(res, req.body, isReady);
 
   if (typeof title !== "string") {
-    isReady = false;
-
     return res
       .status(400)
       .json({ status: 400, message: "Title must be a string" });
   }
 
   if (typeof image !== "string") {
-    isReady = false;
-
     return res
       .status(400)
       .json({ status: 400, message: "Image must be a string" });
   }
 
   if (typeof pages !== "number") {
-    isReady = false;
-
     return res
       .status(400)
       .json({ status: 400, message: "Pages must be a number" });
   }
 
   if (typeof author !== "string") {
-    isReady = false;
-
     return res
       .status(400)
       .json({ status: 400, message: "Author must be a string" });
   }
 
   if (typeof publisher !== "string") {
-    isReady = false;
-
     return res
       .status(400)
       .json({ status: 400, message: "Publisher must be a string" });
   }
 
   if (typeof rating !== "number") {
-    isReady = false;
-
     return res
       .status(400)
       .json({ status: 400, message: "Rating must be a number" });
   }
 
-  if (isReady) {
-    next();
-  }
+  next();
 };
 
 module.exports = { validateBodyReq };
