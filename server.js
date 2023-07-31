a0754a3884524788e1593bec7e0745ad1a42fa16
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2023-07-31 09:32:23 -0300

    style: add .env.example

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const app = require("./app");
+const PORT = process.env.PORT | 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("----- The server is running! -----");
-  console.log("You can access: http://localhost:3000/books");
+  console.log(`You can access: http://localhost:${PORT}/books`);
 });
